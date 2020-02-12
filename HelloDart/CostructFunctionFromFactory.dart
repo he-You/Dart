2ede63e6b611219b5ff3de22435ec9a48ebf3760
heyou <heyou_0423@163.com>
2020-02-12 12:04:15 +0800

    新增:工厂构造法

---
+++ HelloDart/CostructFunctionFromFactory.dart
@@ -0,0 +1,26 @@
+/**
+ * 工厂构造方法：
+ */
+main(List<String> args) {
+  var p1 = Person('heyou');
+  var p2 = Person('heyou');
+  print(identical(p1, p2)); // true
+}
+
+class Person {
+  String name;
+
+  static final Map<String, Person> _cache = <String, Person>{};
+
+  factory Person(String name) {
+    if (_cache.containsKey(name)) {
+      return _cache[name];
+    } else {
+      final p = Person._internal(name);
+      _cache[name] = p;
+      return p;
+    }
+  }
+
+  Person._internal(this.name);
+}
