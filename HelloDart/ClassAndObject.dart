5d787978f553d2e66f3a30875a4d6b578a4acb4b
heyou <heyou_0423@163.com>
2020-02-13 20:59:36 +0800

    更新:static实现类级别的方法和成员变量

--- HelloDart/ClassAndObject.dart
+++ HelloDart/ClassAndObject.dart
@@ -11,6 +11,16 @@
  */
 main(List<String> args) {
   Person()..name="heyou"..eat();
+
+  var stu = Student();
+  stu.name = 'heyou';
+  stu.sno = 110;
+  stu.study();
+
+  Student.time = '早上8点';
+  // stu.time = '早上9点'; 错误做法, 实例对象不能访问类成员
+  Student.attendClass();
+  // stu.attendClass(); 错误做法, 实现对象补鞥呢访问类方法
   
 }
 /**
@@ -21,5 +31,24 @@ class Person {
   String name;
   eat(){
     print("$name在吃");
+  }
+}
+
+/**
+ * 类中定义的成员和方法都属于对象级别的, 在开发中, 我们有时候也需要定义类级别的成员和方法
+ * 在Dart中我们使用static关键字来定义:
+ */
+class Student {
+  String name;
+  int sno;
+
+  static String time;
+
+  study() {
+    print('$name在学习');
+  }
+
+  static attendClass() {
+    print('去上课');
   }
 }
